4ee0a0fb899866560134e8bff51a73aa48b766ec
Chokladglasyr <lim_ida@live.com>
2025-10-06 10:07:22 +0200

    feat: added initial frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import laundryLogo from './assets/logo.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+
+        <a href="https://react.dev" target="_blank">
+          <img src={laundryLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+
+
+    </>
+  )
+}
+
+export default App
