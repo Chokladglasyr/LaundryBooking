d892e55b47e33ac3182bd0709cb8d3435da09695
Chokladglasyr <lim_ida@live.com>
2025-10-07 11:35:29 +0200

    feat: added pg node

--- backend/src/types/auth.ts
+++ backend/src/types/auth.ts
@@ -3,3 +3,21 @@ export type TokenPayload = {
     email: string
     type: string
 }
+export type UserDatabaseModel = {
+    id: string
+    name: string
+    email: string
+    password: string
+    apt_nr: number
+    created_at: string
+}
+export type SignupRequest = {
+    name: string
+    email: string
+    apt_nr: string
+    password: string
+}
+export type LoginRequest = {
+    email: string
+    password: string
+}
