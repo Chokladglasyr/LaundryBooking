a92d1fece4cc1e46a5781684d10be934c0336a87
Chokladglasyr <lim_ida@live.com>
2025-10-15 13:17:24 +0200

    refactor: fixed ruleController to follow new change in runQuery, added try/catch

--- backend/api/controllers/userController.ts
+++ backend/api/controllers/userController.ts
@@ -41,8 +41,8 @@ export async function updateOneUser(
     if (!id) {
       return reply.status(400).send({ message: "Missing parameters." });
     }
-    if (!req.body) {
-      return reply.status(400).send({ message: "No body found." });
+    if (!req.body.email || !req.body.name || !req.body.password) {
+      return reply.status(400).send({ message: "Missing required fields" });
     }
     const userToUpdate = {
       name: req.body.name,
@@ -60,11 +60,11 @@ export async function updateOneUser(
     if (!updatedUser || updatedUser.length === 0) {
       return reply
         .status(404)
-        .send({ message: "Updated user cannot be found." });
+        .send({ message: "Error fetching updated user." });
     }
     reply.status(200).send({ message: "User updated", updatedUser });
   } catch (err) {
-    console.error("Error while updating user: ", err);
+    console.error("Error updating user: ", err);
   }
 }
 
@@ -82,6 +82,6 @@ export async function deleteUser(
     await PostgresConnection.runQuery(text, values);
     reply.status(200).send({ message: `User with id: ${id} is deleted.` });
   } catch (err) {
-    console.error("Error while deleting user: ", err);
+    console.error("Error deleting user: ", err);
   }
 }
