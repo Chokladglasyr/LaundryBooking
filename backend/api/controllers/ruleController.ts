de71ba8828dc01cf3f79c4c17f28c2491fe94cac
Chokladglasyr <lim_ida@live.com>
2025-10-15 09:06:58 +0200

    refactor: cleaned up spaces and order in rulesController and userController

--- backend/api/controllers/ruleController.ts
+++ backend/api/controllers/ruleController.ts
@@ -4,39 +4,61 @@ import PostgresConnection from "../db";
 import { insertRule, updateRule } from "../repository";
 
 export async function getAllRules(req: FastifyRequest, reply: FastifyReply) {
-    const allRules = await PostgresConnection.runQuery(`SELECT * FROM rules;`)
-    reply.status(200).send({message: "All rules fetched", rules: allRules})
+  const allRules = await PostgresConnection.runQuery(`SELECT * FROM rules;`);
+  reply.status(200).send({ message: "All rules fetched", rules: allRules });
 }
 
-export async function createRule(req: FastifyRequest<{Body: rulesAndMsgsRequest}>, reply: FastifyReply) {
-    const newRule = {
-        id: crypto.randomUUID(),
-        title: req.body.title,
-        description: req.body.description
-    }
+export async function getOneRule(
+  req: FastifyRequest<{ Querystring: idRequest }>,
+  reply: FastifyReply
+) {
+  const { id } = req.query;
+  const rule = await PostgresConnection.runQuery(
+    `SELECT * FROM rules WHERE id = '${id}'`
+  );
+  reply.status(200).send({ message: "Rule fetched", rule: rule[0] });
+}
 
-    await insertRule(newRule)
-    const created = await PostgresConnection.runQuery(`SELECT * FROM rules WHERE id = '${newRule.id}'`)
-    reply.status(201).send({message: "New rule created", created_rule: created[0]})
+export async function createRule(
+  req: FastifyRequest<{ Body: rulesAndMsgsRequest }>,
+  reply: FastifyReply
+) {
+  const newRule = {
+    id: crypto.randomUUID(),
+    title: req.body.title,
+    description: req.body.description,
+  };
+  await insertRule(newRule);
+  const created = await PostgresConnection.runQuery(
+    `SELECT * FROM rules WHERE id = '${newRule.id}'`
+  );
+  reply
+    .status(201)
+    .send({ message: "New rule created", created_rule: created[0] });
 }
-export async function getOneRule(req: FastifyRequest<{Querystring: idRequest}>, reply: FastifyReply) {
-    const {id } = req.query
-    const rule = await PostgresConnection.runQuery(`SELECT * FROM rules WHERE id = '${id}'`)
-    reply.status(200).send({message: "Rule fetched", rule: rule[0]})
+
+export async function updateOneRule(
+  req: FastifyRequest<{ Body: rulesAndMsgsRequest; Querystring: idRequest }>,
+  reply: FastifyReply
+) {
+  const { id } = req.query;
+  const ruleToUpdate = {
+    title: req.body.title,
+    description: req.body.description,
+    updated_at: new Date().toISOString(),
+  };
+  await updateRule(ruleToUpdate, id);
+  const updatedRule = await PostgresConnection.runQuery(
+    `SELECT * FROM rules WHERE id = '${id}'`
+  );
+  reply.status(200).send({ message: "Rule updated", rule: updatedRule });
 }
-export async function updateOneRule(req:FastifyRequest<{Body: rulesAndMsgsRequest, Querystring: idRequest}>, reply: FastifyReply) {
-    const {id} = req.query
-    const ruleToUpdate = {
-        title: req.body.title,
-        description: req.body.description,
-        updated_at:  new Date().toISOString(),
-    }
-    await updateRule(ruleToUpdate, id)
-    const updatedRule = await PostgresConnection.runQuery(`SELECT * FROM rules WHERE id = '${id}'`)
-    reply.status(200).send({message: "Rule updated", rule: updatedRule})
-}
-export async function deleteRule(req: FastifyRequest<{Querystring: idRequest}>, reply: FastifyReply) {
-    const { id } = req.query
-    await PostgresConnection.runQuery(`DELETE FROM rules WHERE id = '${id}'`)
-    reply.status(200).send({message: `Rule with id: ${id} deleted`})
+
+export async function deleteRule(
+  req: FastifyRequest<{ Querystring: idRequest }>,
+  reply: FastifyReply
+) {
+  const { id } = req.query;
+  await PostgresConnection.runQuery(`DELETE FROM rules WHERE id = '${id}'`);
+  reply.status(200).send({ message: `Rule with id: ${id} deleted` });
 }
--- backend/api/controllers/userController.ts
+++ backend/api/controllers/userController.ts
@@ -1,40 +1,53 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import PostgresConnection from "../db";
-import { idRequest, rulesAndMsgsRequest } from "../types/requestTypes";
-import { UserDatabaseModel, UserUpdateModel } from "../types/authTypes";
+import { idRequest } from "../types/requestTypes";
+import { UserUpdateModel } from "../types/authTypes";
 import { updateUser } from "../repository";
 
 export async function getAllUsers(req: FastifyRequest, reply: FastifyReply) {
-    const allUsers = await PostgresConnection.runQuery(`SELECT * FROM users;`)
-    reply.status(200).send({message: "Users fetched", users: allUsers})
+  const allUsers = await PostgresConnection.runQuery(`SELECT * FROM users;`);
+  reply.status(200).send({ message: "Users fetched", users: allUsers });
 }
 
-export async function getOneUser(req: FastifyRequest<{Querystring: idRequest}>, reply: FastifyReply) {
+export async function getOneUser(
+  req: FastifyRequest<{ Querystring: idRequest }>,
+  reply: FastifyReply
+) {
+  const { id } = req.query;
 
-    const { id } = req.query
-
-    const user = await PostgresConnection.runQuery(`SELECT * FROM users WHERE id = '${id}'`)
-    reply.status(200).send({message: "User fetched", user: user})
-}
-export async function updateOneUser(req: FastifyRequest<{Querystring:idRequest, Body: UserUpdateModel}>, reply: FastifyReply) {
-    const {id} = req.query
-    const userToUpdate = {
-        name: req.body.name,
-        email: req.body.email,
-        password: await Bun.password.hash(req.body.password, {
-            algorithm: "bcrypt",
-            cost: 10,
-        }),
-        updated_at: new Date().toISOString(),
-    }
-    await updateUser(userToUpdate, id)
-    const updatedUser = await PostgresConnection.runQuery(`SELECT * FROM users WHERE id = '${id}'`)
-    reply.status(200).send({message: "User updated", updatedUser})
+  const user = await PostgresConnection.runQuery(
+    `SELECT * FROM users WHERE id = '${id}'`
+  );
+  reply.status(200).send({ message: "User fetched", user: user });
 }
 
-export async function deleteUser(req: FastifyRequest<{Querystring: idRequest}>, reply: FastifyReply) {
-    const { id } = req.query
+export async function updateOneUser(
+  req: FastifyRequest<{ Querystring: idRequest; Body: UserUpdateModel }>,
+  reply: FastifyReply
+) {
+  const { id } = req.query;
+  const userToUpdate = {
+    name: req.body.name,
+    email: req.body.email,
+    password: await Bun.password.hash(req.body.password, {
+      algorithm: "bcrypt",
+      cost: 10,
+    }),
+    updated_at: new Date().toISOString(),
+  };
+  await updateUser(userToUpdate, id);
+  const updatedUser = await PostgresConnection.runQuery(
+    `SELECT * FROM users WHERE id = '${id}'`
+  );
+  reply.status(200).send({ message: "User updated", updatedUser });
+}
 
-    await PostgresConnection.runQuery(`DELETE FROM users WHERE id = '${id}'`)
-    reply.status(200).send({message: `User with id: ${id} is deleted.`})
+export async function deleteUser(
+  req: FastifyRequest<{ Querystring: idRequest }>,
+  reply: FastifyReply
+) {
+  const { id } = req.query;
+
+  await PostgresConnection.runQuery(`DELETE FROM users WHERE id = '${id}'`);
+  reply.status(200).send({ message: `User with id: ${id} is deleted.` });
 }
