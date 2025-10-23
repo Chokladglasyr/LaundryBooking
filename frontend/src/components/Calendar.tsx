073808fb1f65eb82045feda8113549c866c9f74d
Chokladglasyr <lim_ida@live.com>
2025-10-23 10:31:39 +0200

    feat: added styles to selected day, and added so the days that has already passed are greyed out

--- frontend/src/components/Calendar.tsx
+++ frontend/src/components/Calendar.tsx
@@ -21,7 +21,10 @@ function Calendar() {
   const currentDate = new Date();
   const [currentMonth, setCurrentMonth] = useState(currentDate.getMonth());
   const [currentYear, setCurrentYear] = useState(currentDate.getFullYear());
-  const [selectedDate, setSelectedDate] = useState(currentDate)
+  const [selectedDate, setSelectedDate] = useState(currentDate);
+  
+  const today = new Date()
+  today.setHours(0, 0, 0, 0)
 
   const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
 
@@ -29,39 +32,41 @@ function Calendar() {
   const sweFirstDayOfMonth = (firstDayOfMonth + 6) % 7;
 
   const prevMonth = () => {
-    setCurrentMonth((prevMonth) =>(prevMonth === 0 ? 11 : prevMonth -1))
-    setCurrentYear((prevYear) =>(currentMonth === 11 ? prevYear -1 : prevYear))
-  }
+    setCurrentMonth((prevMonth) => (prevMonth === 0 ? 11 : prevMonth - 1));
+    setCurrentYear((prevYear) =>
+      currentMonth === 11 ? prevYear - 1 : prevYear
+    );
+  };
   const nextMonth = () => {
-    setCurrentMonth((prevMonth) => (prevMonth === 11 ? 0 : prevMonth +1))
-    setCurrentYear((prevYear) => (currentMonth === 11 ? prevYear +1 : prevYear))
-  }
+    setCurrentMonth((prevMonth) => (prevMonth === 11 ? 0 : prevMonth + 1));
+    setCurrentYear((prevYear) =>
+      currentMonth === 11 ? prevYear + 1 : prevYear
+    );
+  };
 
   const handleDatePick = (day: number) => {
-    const pickedDate = new Date(currentYear, currentMonth, day)
-    const today = new Date()
+    const pickedDate = new Date(currentYear, currentMonth, day);
 
-    if(pickedDate >= today){
-        setSelectedDate(pickedDate)
+    if (pickedDate >= today) {
+      setSelectedDate(pickedDate);
     }
-  }
-  console.log(selectedDate)
+  };
+  console.log(selectedDate);
   return (
     <>
       <div className="calendar-app">
         <div className="calendar">
           <div className="navigate-date">
-              <button onClick={prevMonth} className="left">
-                <img src={LeftArrow} alt="left arrows" />{" "}
-              </button>
+            <button onClick={prevMonth} className="left">
+              <img src={LeftArrow} alt="left arrows" />{" "}
+            </button>
             <div className="heading">
-            <h2 className="month">{monthsOfYear[currentMonth]}</h2>
-            <h2 className="year">{currentYear}</h2>
-
+              <h2 className="month">{monthsOfYear[currentMonth]}</h2>
+              <h2 className="year">{currentYear}</h2>
             </div>
-              <button onClick={nextMonth} className="right">
-                <img src={RightArrow} alt="right arrows" />
-              </button>
+            <button onClick={nextMonth} className="right">
+              <img src={RightArrow} alt="right arrows" />
+            </button>
           </div>
           <div className="weekdays">
             {daysOfWeek.map((day, index) => (
@@ -73,7 +78,25 @@ function Calendar() {
               <span key={`empty-${index}`} />
             ))}
             {[...Array(daysInMonth).keys()].map((day, index) => (
-              <button onClick={() => handleDatePick(day+1)} className={new Date(currentYear, currentMonth, day+1).getTime() === selectedDate.getTime() ? 'valid-days-selected' : 'valid-days'} id={day + 1 === currentDate.getDate() && currentMonth === currentDate.getMonth() && currentYear === currentDate.getFullYear() ? 'current-day' : ''} key={index + 1}>
+              <button
+                onClick={() => handleDatePick(day + 1)}
+                className={
+                  new Date(currentYear, currentMonth, day + 1).getTime() ===
+                  selectedDate.getTime()
+                    ? "valid-days-selected"
+                    : new Date(currentYear, currentMonth, day + 1) >= today
+                      ? "valid-days"
+                      : "invalid-days"
+                }
+                id={
+                  day + 1 === currentDate.getDate() &&
+                  currentMonth === currentDate.getMonth() &&
+                  currentYear === currentDate.getFullYear()
+                    ? "current-day"
+                    : ""
+                }
+                key={index + 1}
+              >
                 {day + 1}
               </button>
             ))}
@@ -81,12 +104,12 @@ function Calendar() {
         </div>
       </div>
       <div className="booking-container">
-      <div className="timeslots-container">
-        <div className="timeslot-1">8-12</div>
-        <div className="timeslot-2">12-17</div>
-        <div className="timeslot-3">17-21</div>
-      </div>
-            <button className="primary-btn">BOKA</button>
+        <div className="timeslots-container">
+          <button className="timeslot-1">8-12</button>
+          <button className="timeslot-2">12-17</button>
+          <button className="timeslot-3">17-21</button>
+        </div>
+        <button className="primary-btn">BOKA</button>
       </div>
     </>
   );
