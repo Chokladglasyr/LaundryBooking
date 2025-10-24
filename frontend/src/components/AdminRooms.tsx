e1b70b67983e18de9fb91adc24aea4c43fd62e4d
Chokladglasyr <lim_ida@live.com>
2025-10-24 13:44:54 +0200

    refactor: adminrooms changed classnames and added handleinput on change

--- frontend/src/components/AdminRooms.tsx
+++ frontend/src/components/AdminRooms.tsx
@@ -1,24 +1,36 @@
+import { useState } from "react";
+
 function AdminRooms() {
-    const rooms = [
-        {
-            id: '1',
-            name: 'Tvättstuga 1',
-            description: '1 torkrum, 2 tvättmaskiner och en torktumlare, aojdjadjajsdoj jandöjasöjjfaji aöojjafjfs aojfajsj jaa'
-        },
-        {
-            id: '2',
-            name: 'Tvättstuga 2',
-            description: '1 torkrum, 2 tvättmaskiner och en torktumlare, aojdjadjajsdoj jandöjasöjjfaji aöojjafjfs aojfajsj jaa'
-        },
-        {
-            id: '3',
-            name: 'Tvättstuga 3',
-            description: '1 torkrum, 2 tvättmaskiner och en torktumlare, aojdjadjajsdoj jandöjasöjjfaji aöojjafjfs aojfajsj jaa'
-        },
-    ]
-    return(
-        <>
-<article>
+  const [formData, setFormData] = useState({});
+  const rooms = [
+    {
+      id: "1",
+      name: "Tvättstuga 1",
+      description:
+        "1 torkrum, 2 tvättmaskiner och en torktumlare, aojdjadjajsdoj jandöjasöjjfaji aöojjafjfs aojfajsj jaa",
+    },
+    {
+      id: "2",
+      name: "Tvättstuga 2",
+      description:
+        "1 torkrum, 2 tvättmaskiner och en torktumlare, aojdjadjajsdoj jandöjasöjjfaji aöojjafjfs aojfajsj jaa",
+    },
+    {
+      id: "3",
+      name: "Tvättstuga 3",
+      description:
+        "1 torkrum, 2 tvättmaskiner och en torktumlare, aojdjadjajsdoj jandöjasöjjfaji aöojjafjfs aojfajsj jaa",
+    },
+  ];
+  const handleInput = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+  return (
+    <>
+      <article>
         <form action="" id="room-form">
           <label htmlFor="room-form">Ny tvättstuga: </label>
           <input
@@ -27,9 +39,10 @@ function AdminRooms() {
             name="name"
             id="name"
             placeholder="Namn"
+            onChange={handleInput}
           />
-          <textarea name="description" id="description" rows={8}></textarea>
-          <button id="create-room" className="primary-btn">
+          <textarea name="description" id="description" rows={8} onChange={handleInput}></textarea>
+          <button id="create-room" className="primary-btn-green">
             SPARA
           </button>
         </form>
@@ -44,22 +57,22 @@ function AdminRooms() {
               name="title"
               id="title"
               value={room.name}
+              onChange={handleInput}
             />
-            <textarea name="description" id="description" rows={8}>
-              {room.description}
+            <textarea name="description" id="description" rows={8} value={room.description} onChange={handleInput}>
             </textarea>
             <div className="btn-container">
-              <button id="edit-room" className="primary-btn">
+              <button id={`edit-room-${index}`} className="primary-btn-green">
                 SPARA
               </button>
-              <button id="delete-room" className="primary-btn">
+              <button id={`delete-room-${index}`} className="primary-btn-red">
                 RADERA
               </button>
             </div>
           </form>
         ))}
       </article>
-        </>
-    )
+    </>
+  );
 }
-export default AdminRooms
+export default AdminRooms;
