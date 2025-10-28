6a79b87dbc5fafbd58665c2fb3fc6c1ab989e3cc
Chokladglasyr <lim_ida@live.com>
2025-10-28 13:51:11 +0100

    feat: fetches room for booking.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./components/Login";
 import Landing from "./components/Landing";
-import { UserContext, type User } from "./store/types";
+import { type User } from "./store/types";
 import axios from "axios";
 
 function App() {
@@ -32,11 +32,9 @@ function App() {
 
   return (
     <>
-      <UserContext.Provider value={{ user, setUser }}>
         {loggedIn && <Landing />}
         {!loggedIn && <Login setLoggedIn={setLoggedIn} setUser={setUser} />}
         <button onClick={logout}>Logout</button>
-      </UserContext.Provider>
     </>
   );
 }
--- frontend/src/components/Booking.tsx
+++ frontend/src/components/Booking.tsx
@@ -1,24 +1,32 @@
-import { useContext } from "react";
-import { UserContext } from "../store/types";
+import { useEffect, useState } from "react";
+import { type Rooms } from "../store/types";
 import Calendar from "./Calendar";
 import ChooseRoom from "./ChooseRoom";
+import axios from "axios";
 
 function Booking() {
-  const user = useContext(UserContext);
-  console.log(user);
+  const [room, setRoom] = useState<Rooms | null>(null);
   const params = new URLSearchParams(document.location.search);
   const room_id = params.get("id");
-  const room = {
-    description:
-      "1 torkrum, 2 tvättmaskiner och en torktumlare, aojdjadjajsdoj jandöjasöjjfaji aöojjafjfs aojfajsj jaa",
-  };
+  useEffect(() => {
+    async function getRoom() {
+        if(!room_id) return
+      const res = await axios.get(`room?id=${room_id}`, {
+          withCredentials: true,
+        });
+        console.log(res.data.room[0])
+      setRoom(res.data.room[0]);
+    }
+    getRoom();
+  },[room_id]);
+  console.log(room?.description)
   return (
     <>
       <div className="landing" id="booking">
         <div className="description-container">
           <article className="room-details">
-            <h1>{`Tvättstuga ${room_id}`}</h1>
-            <p>{room.description}</p>
+            <h1>{room?.name}</h1>
+            <p>{room?.description}</p>
           </article>
           <ChooseRoom />
         </div>
