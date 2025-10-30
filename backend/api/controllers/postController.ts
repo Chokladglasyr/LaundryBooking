daee56adb6efde15a0dd5f27586eb00772e076fa
Chokladglasyr <lim_ida@live.com>
2025-10-30 10:15:29 +0100

    refactor: changing word messages to post instead, in backend

--- backend/api/controllers/postController.ts
+++ backend/api/controllers/postController.ts
@@ -1,18 +1,18 @@
 import { FastifyReply, FastifyRequest } from "fastify";
-import { idRequest, rulesAndMsgsRequest } from "../types/requestTypes";
-import { insertpost, updatepost } from "../repository";
+import { idRequest, rulesAndPostsRequest } from "../types/requestTypes";
+import { insertPost, updatePost } from "../repository";
 import postgresConnection from "../db";
 
-export async function getAllposts(req: FastifyRequest, reply: FastifyReply) {
+export async function getAllPosts(req: FastifyRequest, reply: FastifyReply) {
   try {
     const text = `SELECT * FROM posts`;
-    const allposts = await postgresConnection.runQuery(text);
-    if (!allposts || allposts.length === 0) {
-      reply.status(404).send({ post: "No posts found." });
+    const allPosts = await postgresConnection.runQuery(text);
+    if (!allPosts || allPosts.length === 0) {
+      reply.status(404).send({ message: "No posts found." });
     }
     reply
       .status(200)
-      .send({ post: "posts fetched", posts: allposts });
+      .send({ message: "posts fetched", posts: allPosts });
   } catch (err) {
     console.error("Error fetching posts.");
   }
@@ -24,94 +24,94 @@ export async function getOnePost(
 ) {
   const { id } = req.query;
   if (!id) {
-    reply.status(400).send({ post: "Missing parameters." });
+    reply.status(400).send({ message: "Missing parameters." });
   }
   const text = `SELECT * FROM posts WHERE id = $1`;
   const values = [id];
   const post = await postgresConnection.runQuery(text, values);
   if (!post || post.length === 0) {
-    reply.status(404).send({ post: `No post found with id: ${id}` });
+    reply.status(404).send({ message: `No post found with id: ${id}` });
   }
   reply
     .status(200)
-    .send({ post: "post fetched", fetched_post: post[0] });
+    .send({ message: "post fetched", post: post[0] });
 }
 
 export async function createPost(
-  req: FastifyRequest<{ Body: rulesAndMsgsRequest }>,
+  req: FastifyRequest<{ Body: rulesAndPostsRequest }>,
   reply: FastifyReply
 ) {
   try {
     if (!req.body.title || !req.body.description) {
-      reply.status(400).send({ post: "Missing required fields." });
+      reply.status(400).send({ message: "Missing required fields." });
     }
-    const newMsg = {
+    const newPost = {
       id: crypto.randomUUID(),
       title: req.body.title,
       description: req.body.description,
     };
-    await insertpost(newMsg);
+    await insertPost(newPost);
     const text = `SELECT * FROM posts WHERE id = $1`;
-    const values = [newMsg.id];
+    const values = [newPost.id];
     const created = await postgresConnection.runQuery(text, values);
     if (!created || created.length === 0) {
-      return reply.status(404).send({ post: "Error fetching new post." });
+      return reply.status(404).send({ message: "Error fetching new post." });
     }
     reply
       .status(201)
-      .send({ post: "New post created", created_post: created[0] });
+      .send({ message: "New post created", post: created[0] });
   } catch (err) {
     console.error("Error inserting new post: ", err);
   }
 }
 
-export async function updateOnepost(
-  req: FastifyRequest<{ Body: rulesAndMsgsRequest; Querystring: idRequest }>,
+export async function updateOnePost(
+  req: FastifyRequest<{ Body: rulesAndPostsRequest; Querystring: idRequest }>,
   reply: FastifyReply
 ) {
   try {
     const { id } = req.query;
     if (!id) {
-      reply.status(400).send({ post: "Missing parameters." });
+      reply.status(400).send({ message: "Missing parameters." });
     }
     if (!req.body.title || !req.body.description) {
-      reply.status(400).send({ post: "Missing required fields." });
+      reply.status(400).send({ message: "Missing required fields." });
     }
     const postToUpdate = {
       title: req.body.title,
       description: req.body.description,
       updated_at: new Date().toISOString(),
     };
-    await updatepost(postToUpdate, id);
+    await updatePost(postToUpdate, id);
     const text = `SELECT * FROM posts WHERE id = $1`;
     const values = [id];
-    const updatedpost = await postgresConnection.runQuery(text, values);
-    if (!updatedpost || updatedpost.length === 0) {
-      reply.status(404).send({ post: "Updated post cannot be found." });
+    const updatedPost = await postgresConnection.runQuery(text, values);
+    if (!updatedPost || updatedPost.length === 0) {
+      reply.status(404).send({ message: "Updated post cannot be found." });
     }
     reply
       .status(200)
-      .send({ post: "post updated", updated_post: updatedpost });
+      .send({ message: "Post updated", updated_post: updatedPost });
   } catch (err) {
     console.error("Error updating post: ", err);
   }
 }
 
-export async function deletepost(
+export async function deletePost(
   req: FastifyRequest<{ Querystring: idRequest }>,
   reply: FastifyReply
 ) {
   try {
     const { id } = req.query;
     if (!id) {
-      reply.status(400).send({ post: "Missing parameters." });
+      reply.status(400).send({ message: "Missing parameters." });
     }
     await postgresConnection.runQuery(
       `DELETE FROM posts WHERE id = '${id}'`
     );
     reply
       .status(200)
-      .send({ post: `post with id: ${id} was deleted succesfully.` });
+      .send({ message: `Post with id: ${id} was deleted succesfully.` });
   } catch (err) {
     console.error("Error deleting post: ", err);
   }
