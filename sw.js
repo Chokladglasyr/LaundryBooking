678fa6e9be7f68de7a7fa6b90f68e2286cf2be00
Chokladglasyr <lim_ida@live.com>
2025-11-03 13:34:15 +0100

    feat: added service worker

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "laundry-booking-v1"
+
+const CACHED_FILES = ["/index.html", "/manifest.json"]
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME)).then((cache) => {
+        cache.addAll(CACHED_FILES)
+    })
+})
+
+self.addEventListener('activate', (e) => {
+    cache.keys().then((cacheNames) => {
+        cacheNames.forEach((name) => {
+            if(name !== CACHE_NAME) {
+                caches.delete(name)
+            }
+        })
+    })
+})
+
+self.addEventListener('fetch', async (e) => {
+    const {request} = e
+
+    try{
+        const networkResponse = await fetch(request)
+        if(networkResponse.ok) {
+            return networkResponse
+        }
+    } catch(err) {
+        const cachedReponse = await caches.match(request)
+        if(cachedReponse) {
+            return cachedReponse
+        }
+    }
+})
+
