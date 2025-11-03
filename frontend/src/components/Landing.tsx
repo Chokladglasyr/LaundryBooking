7875ec8655325df8070d03f12b0dcb2f2c3343fb
Chokladglasyr <lim_ida@live.com>
2025-11-03 11:47:20 +0100

    feat: refactor: formatted

--- frontend/src/components/Landing.tsx
+++ frontend/src/components/Landing.tsx
@@ -20,13 +20,13 @@ function Landing() {
     }
   };
   const location = useLocation();
-    const logout = async () => {
+  const logout = async () => {
     try {
-      await axios.get('/logout', {withCredentials: true})
-      navigate('/')
-    }catch(err) {
-      if(err instanceof Error) {
-        console.error("Failed to logout: ", err)
+      await axios.get("/logout", { withCredentials: true });
+      navigate("/");
+    } catch (err) {
+      if (err instanceof Error) {
+        console.error("Failed to logout: ", err);
       }
     }
   };
@@ -41,7 +41,7 @@ function Landing() {
       </nav>
       <Outlet />
       {location.pathname === "/home" && <Home />}
-            <button onClick={logout}>Logout</button>
+      <button onClick={logout}>Logout</button>
     </>
   );
 }
