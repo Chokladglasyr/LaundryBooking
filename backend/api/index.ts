45a1a6529e60999539f1df40e2aa17e78877b915
Chokladglasyr <lim_ida@live.com>
2025-11-08 23:19:32 +0100

    feat: added cronjobs for deleting old entries in database for bookings and reset request

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -4,6 +4,8 @@ import auth from "./auth";
 import PostgresConnection from "./db";
 import fastifyCors from "@fastify/cors";
 import fastifyCookie, { FastifyCookieOptions } from "@fastify/cookie";
+import fastifySchedule from "@fastify/schedule";
+import { cleanUpBooking, cleanUpReset, job } from "./jobs";
 
 const app = fastify({});
 
@@ -22,22 +24,32 @@ const start = async () => {
     await PostgresConnection.initTables();
     await PostgresConnection.createAdmin();
 
+    await app.register(fastifySchedule)
     await app.register(fastifyCookie, {
       parseOptions: {},
     });
     await app.register(auth);
     await app.register(routes, {});
 
+    app.ready().then(() => {
+      app.scheduler.addSimpleIntervalJob(cleanUpBooking)
+      console.log("Cleanup for bookings in database set for every week.")
+
+      app.scheduler.addSimpleIntervalJob(cleanUpReset)
+      console.log("Cleanup for reset requests in database set for every second day")
+    })
     if (process.env.NODE_ENV !== "production") {
       const PORT = Number(process.env.PORT) || 3000;
       await app.listen({ port: PORT, host: "0.0.0.0" });
       console.log("Listening on port ", PORT);
     }
+
   } catch (err) {
     console.error("Server failed: ", err);
     process.exit(1);
   }
 };
+
 
 setInterval(
   async () => {
